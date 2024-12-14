9c77b80963a21ecc7b792358a08a6f6635f85809
ErnestoMiguel <perezpupoernestomiguel@gmail.com>
2024-12-14 11:07:06 -0300

    Update Tabla_de_multiplicacion.py

--- Tabla_de_multiplicacion.py
+++ Tabla_de_multiplicacion.py
@@ -2,4 +2,4 @@ numero = int(input("Que tabla de Multiplicar quieres ver: "))
 print(f"La tabla de multiplicar del {numero} es : ")
 for i in range(0, 11):
     print(f"{numero} x {i} = {numero*i}")
-print("Tu Puedes 2")
+print("Tu Puedes 32")
